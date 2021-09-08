f4a20121c5575ad18e9654265ce14d022098dfa3
sycapple <89971677+sycapple@users.noreply.github.com>
2021-09-08 22:51:34 +0800

    Add files via upload

--- test9.8.c
+++ test9.8.c
@@ -1,26 +1,28 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
-struct person
-{
-	char name[20];
-	short tall;
-	short weight;
-	short age;
-
-};
-int main()
-{
-	struct person syc = {"Ê©Óî³¬",173,66,18};
-	//printf("%s ", syc.name);
-	//printf("%d ", syc.tall);
-	//printf("%d ", syc.weight);
-	//printf("%d ", syc.age);
-
-
-	struct person*  dz = &syc;
-		printf("%s\n", (*dz).name);
-		printf("%s\n", dz->name);
-	
-
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//#include <string.h>
+//struct person
+//{
+//	char name[20];
+//	short tall;
+//	short weight;
+//	short age;
+//
+//};
+//int main()
+//{
+//	struct person syc = {"Ê©Óî³¬",173,66,18};
+//	//printf("%s ", syc.name);
+//	//printf("%d ", syc.tall);
+//	//printf("%d ", syc.weight);
+//	//printf("%d ", syc.age);
+//
+//
+//	struct person*  dz = &syc;
+//	strcpy(dz->name, "ÇØ³ç");
+//		printf("%s\n", (*dz).name);
+//		printf("%s\n", dz->name);
+//	
+//
+//	return 0;
+//}
---
+++ 秦崇是傻逼.c
@@ -0,0 +1,18 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	printf("秦崇是傻逼吗？？？（灵魂拷问）\n");
+//	scanf("%d", &a);
+//	if (a == 1)
+//	{
+//		printf("你TND真是个天才！！！\n");
+//	}
+//	else
+//	{
+//		printf("很遗憾，他TMD就是个傻逼！！\n");
+//
+//	}
+//	return 0;
+//}
