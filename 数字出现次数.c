3db26ccc9363a1e98c9b15274672f3899672c586
sycapple <89971677+sycapple@users.noreply.github.com>
2021-09-17 18:41:41 +0800

    Add files via upload

---
+++ 数字出现次数.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int a[20] = { 1,1,2,2,3,3,4,4,5,5,6,6,7,8 };
+//int main()
+//{
+//	int b[100] = { 0 };
+//	int i = 0;
+//	for (; i <= 20; i++)
+//	{
+//		b[a[i]]++;
+//	}
+//	i = 0;
+//	for (; i <= 100; i++)
+//	{
+//		if (b[i] == 1)
+//			printf("数字%d出现了一次\n", i);
+//	}
+//	return 0;
+//}
